e9960ac1bfbff280a4dc09af1beeb5b3eb7524da
tomeliuye <tomliuye@gmail.com>
2014-10-28 10:57:00 +0800

    solution for zigzag convention

---
+++ src/solution/ZigZagConvention.java
@@ -0,0 +1,66 @@
+package solution;
+
+/*
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string text, int nRows);
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+*/
+
+public class ZigZagConvention {
+
+    public String solution(String s, int nRows) {
+
+        if (nRows <= 1) {
+            return s;
+        }
+
+        int len = s.length();
+
+        if (len <= nRows) {
+            return s;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int step = 2 * nRows - 2;
+
+        for (int i = 0; i < nRows; i++) {
+            int j = i;
+            while (j < len) {
+                if (i == 0 || i == nRows - 1) {
+                    sb.append(s.charAt(j));
+                    j += step;
+                    continue;
+                } else {
+                    if (j == i) {
+                        sb.append(s.charAt(j));
+                    } else {
+                        sb.append(s.charAt(j - 2*i));
+                        sb.append(s.charAt(j));
+                    }
+                }
+
+                j += step;
+
+                if (j >= len && j - 2*i < len) {
+                    sb.append(s.charAt(j - 2*i));
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String args[]) {
+        ZigZagConvention zigzag = new ZigZagConvention();
+
+        System.out.println(zigzag.solution("ABCDE", 4));
+    }
+}
