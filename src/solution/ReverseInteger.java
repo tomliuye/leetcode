798ea4dd8ed18f8d7ac9939d5f32f482aa4e6229
tomeliuye <tomliuye@gmail.com>
2014-10-28 17:52:15 +0800

    solution for reverse integer

---
+++ src/solution/ReverseInteger.java
@@ -0,0 +1,33 @@
+package solution;
+
+/*
+ Reverse digits of an integer.
+
+ Example1: x = 123, return 321
+ Example2: x = -123, return -321
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+
+        StringBuilder sb = new StringBuilder();
+
+        if (x < 0) {
+            sb.append("-");
+        }
+
+        x = Math.abs(x);
+
+        //采用取余数的方法
+        while (x > 0) {
+            int i = x % 10;
+            sb.append(i);
+            x = x/10;
+        }
+        
+        try {
+            return Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
