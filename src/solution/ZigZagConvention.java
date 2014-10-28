6cd64781ebe2ab3a33bed7f3492a390c09b62121
tomeliuye <tomliuye@gmail.com>
2014-10-28 11:05:06 +0800

    add comment for zigzag solution

--- src/solution/ZigZagConvention.java
+++ src/solution/ZigZagConvention.java
@@ -17,31 +17,37 @@ public class ZigZagConvention {
 
     public String solution(String s, int nRows) {
 
+        //合法性判断
         if (nRows <= 1) {
             return s;
         }
 
         int len = s.length();
 
+        //前置判断
         if (len <= nRows) {
             return s;
         }
 
         StringBuilder sb = new StringBuilder();
 
+        //分析得出每个zigzag的步长
         int step = 2 * nRows - 2;
 
         for (int i = 0; i < nRows; i++) {
             int j = i;
             while (j < len) {
+                //波峰波谷只有一个占位
                 if (i == 0 || i == nRows - 1) {
                     sb.append(s.charAt(j));
                     j += step;
                     continue;
                 } else {
                     if (j == i) {
+                        //最左侧
                         sb.append(s.charAt(j));
                     } else {
+                        //除左侧以外，有2个元素
                         sb.append(s.charAt(j - 2*i));
                         sb.append(s.charAt(j));
                     }
@@ -49,6 +55,7 @@ public class ZigZagConvention {
 
                 j += step;
 
+                //未完成的zigzag补完
                 if (j >= len && j - 2*i < len) {
                     sb.append(s.charAt(j - 2*i));
                 }
